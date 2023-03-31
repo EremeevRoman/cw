ff56d6388d460e0a2ffddeffd2dc83e0013c5bfe
Roman <romaneremeev7@mail.ru>
2023-03-31 19:14:16 +0300

    Final commit

--- src/Employee.java
+++ src/Employee.java
@@ -2,14 +2,15 @@ public class Employee {
     private String name;
     private int department;
     private int salary;
-    private int id;
-    private static int counter=1;
-    public Employee(String name, int department, int salary, int id) {
-        this.name =name;
-        this.department=department;
-        this.salary=salary;
-        this.id=counter;
-        counter++;
+    private static int id;
+    private static int counter = 1;
+
+    public Employee(String name, int department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        id = counter++;
+
     }
 
     public String getName() {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    public static int generateNumber(int max) {
+        java.util.Random random = new java.util.Random();
+        return random.nextInt(1, max);
     }
+
+    public static void main(String[] args) {
+        Employee[] emp = new Employee[10];
+        emp[0] = new Employee("Захаров Фрол Созонович", generateNumber(5), generateNumber(100000));
+        emp[1] = new Employee("Молчанов Арсен Альвианович", generateNumber(5), generateNumber(100000));
+        emp[2] = new Employee("Морозов Игорь Георгьевич", generateNumber(5), generateNumber(100000));
+        emp[3] = new Employee("Овчинников Андрей Никитевич", generateNumber(5), generateNumber(100000));
+        emp[4] = new Employee("Бобров Казимир Павлович", generateNumber(5), generateNumber(100000));
+        emp[5] = new Employee("Яковлев Тимур Павлович", generateNumber(5), generateNumber(100000));
+        emp[6] = new Employee("Зиновьев Владимир Куприянович", generateNumber(5), generateNumber(100000));
+        emp[7] = new Employee("Игнатьев Григорий Авксентьевич", generateNumber(5), generateNumber(100000));
+        emp[8] = new Employee("Доронин Устин Алексеевич", generateNumber(5), generateNumber(100000));
+        emp[9] = new Employee("Балтабай Алибек Нарутович", generateNumber(5), generateNumber(100000));
+        int totalSalary = 0;
+        int maxSalary = Integer.MIN_VALUE;
+        int minSalary = Integer.MAX_VALUE;
+        for (Employee r : emp) {
+            if (r.getSalary() > maxSalary) {
+                maxSalary = r.getSalary();
+            }
+            if (r.getSalary() < minSalary) {
+                minSalary = r.getSalary();
+            }
+            totalSalary = totalSalary + r.getSalary();
+            System.out.println(r.getName() + " " + r.getSalary() + " рублей " + r.getDepartment() + "  отдел");
+
+        }
+        float averageSalary = totalSalary / 10f;
+        System.out.println(averageSalary + " Средняя зарплата");
+        System.out.println(maxSalary + " Максимальная зарплата");
+        System.out.println(minSalary + " Минимальная зарплата");
+        System.out.println(totalSalary + " Общая сумма затрат на зарплату");
+
+
+    }
+
 }
